6d8efb30ae7f191b06e40c507c3e68144212a3ac
Matheus Santos Pena <mathewwsp@hotmail.com>
2019-05-06 16:58:37 -0300

    Add files via upload

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title></title>
+</head>
+<body>
+<?php
+
+//atribui um valor a uma variável x e depois adiciona 20
+$x = 100;
+$x = $x + 20;
+echo $x;
+echo "<br>";
+//o mesmo que o anterior, com menos código
+$y = 100;
+$y += 20;
+echo $y;
+
+?> 
+</body>
+</html>
